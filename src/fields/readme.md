576fc7ee4d5a304ae79782033c2fb6e7f3e02a39
Sadnan Saquif <SSaquif@users.noreply.github.com>
2022-01-31 06:17:23 -0500

    Accessible Input Component

---
+++ src/fields/readme.md
@@ -0,0 +1,13 @@
+# Input
+
+This is an `Accessible Compound Component`, since it is made of 2 components
+
+## General
+
+- The unique id shared by both Label and Input when wrapped by Field, allows screenreaders to correctly read the labels
+- If Lable or Input is not wrapped by Field, then id is undefined
+
+## Things to Investigate Furthur
+
+- Why is it not necessary to do a JSX children pass through for the label tag `return <label ref={ref} htmlFor={id} {...props}>{children}</label>`. But we had to do it for the `<button>`
+  - <details>I think rendering children is probaly native behvior, maybe can skip it in button too?</details>
---
+++ src/fields/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { transparentize } from 'polished';
+import {
+  LIGHT_COLOR,
+  DARK_COLOR,
+  DARK_TEXT_COLOR,
+  DISABLED_OPACITY,
+} from '../utils/styles';
+
+export const StyledLabel = styled.label`
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+`;
+
+export const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  padding: 0.5rem 0.4rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: ${DARK_TEXT_COLOR};
+  background-color: transparent;
+  border: 1px solid ${DARK_COLOR};
+  appearance: none;
+  border-radius: 0;
+  transition: all 0.15s ease-in-out;
+
+  &:focus {
+    outline: none;
+    border-color: ${LIGHT_COLOR};
+    box-shadow: 0 0 0 0.2rem ${transparentize(0.75, LIGHT_COLOR)};
+  }
+
+  &:disabled {
+    background-color: ${DARK_COLOR};
+    opacity: ${DISABLED_OPACITY};
+  }
+`;
